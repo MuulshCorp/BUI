2856d76b52cb01252d2ac349f87f2bfb0e76a643
Slacker Company <38662305+MuulshCorp@users.noreply.github.com>
2018-10-24 12:17:29 +0200

    Commandes

--- commands/serveur.js
+++ commands/serveur.js
@@ -6,6 +6,9 @@ if (command == "serveur") {
   console.log('error:', error); // Print the error if one occurred
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
   console.log('body:', body); // Print the HTML for the Google homepage.
+  if (response && response.statusCode == "404") {
+  	message.channel.send('Le serveur est fermé')
+  }
 });
 }
 
