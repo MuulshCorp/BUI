bfc93539bf3d231b83b106201ad02ba4ab4c75bb
Slacker Company <38662305+MuulshCorp@users.noreply.github.com>
2018-10-24 12:07:15 +0200

    Commandes

--- commands/avatar.js
+++ commands/avatar.js
@@ -2,12 +2,7 @@ const Discord = require("discord.js");
 module.exports.run = async (client, message, args, command, config, fs, request) => {
 //mettre la commande ici
 if (command == 'avatar') {
-	let member = message.mentions.members.first() || message.guild.members.get(args[0]);
-    if(!empty(member)) {
-    	message.reply(member.avatarURL);
-    } else {
-    	message.reply(message.author.avatarURL);
-    }
+	message.reply(message.author.avatarURL);
 }
 //fin
 }
