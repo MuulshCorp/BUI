6e71eb5de63abe0a7584266a8698597f9f140585
Slacker Company <38662305+MuulshCorp@users.noreply.github.com>
2018-10-24 12:28:20 +0200

    Commandes

--- commands/serveur.js
+++ commands/serveur.js
@@ -3,9 +3,9 @@ module.exports.run = async (client, message, args, command, config, fs, request)
 //mettre la commande ici
 if (command == "serveur") {
 	request('http://lescookies.ml/stats', function (error, response, body) {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
+  console.log('error:', error); 
+  console.log('statusCode:', response && response.statusCode); 
+  console.log('body:', body); 
   if (response && response.statusCode == "404") {
   	message.channel.send('Le serveur est fermé');
   } else {
