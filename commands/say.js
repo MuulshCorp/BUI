56bb50aad2bf9d0a1d3eb5e70e92f56bf2c504a2
Slacker Company <38662305+MuulshCorp@users.noreply.github.com>
2018-10-24 21:37:52 +0200

    Commandes

--- commands/say.js
+++ commands/say.js
@@ -2,11 +2,13 @@ const Discord = require("discord.js");
 module.exports.run = async (client, message, args, command, config, fs, request) => {
 //mettre la commande ici
 if(command == "say") {
-	if (message.author == "372778933839593483")
+	if (message.author == "372778933839593483") {
 		return message.channel.send('Error');
+	} else {
     const sayMessage = args.join(" ");
     message.delete().catch(O_o=>{}); 
     message.channel.send(sayMessage);
+	}
 }
 //fin
 }
