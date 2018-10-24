cb6edfa74d2acebe7cd450a7fc4f9458f8aa603f
Slacker Company <38662305+MuulshCorp@users.noreply.github.com>
2018-10-24 12:05:34 +0200

    Commandes

--- commands/avatar.js
+++ commands/avatar.js
@@ -3,7 +3,7 @@ module.exports.run = async (client, message, args, command, config, fs, request)
 //mettre la commande ici
 if (command == 'avatar') {
 	let member = message.mentions.members.first() || message.guild.members.get(args[0]);
-    if(empty(member)) {
+    if(!empty(member)) {
     	message.reply(member.avatarURL);
     } else {
     	message.reply(message.author.avatarURL);
