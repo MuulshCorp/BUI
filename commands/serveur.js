74764f45584d19014578daab5d4fd28cbed7366a
Slacker Company <38662305+MuulshCorp@users.noreply.github.com>
2018-10-24 12:13:23 +0200

    Commandes

---
+++ commands/serveur.js
@@ -0,0 +1,16 @@
+const Discord = require("discord.js");
+module.exports.run = async (client, message, args, command, config, fs, request) => {
+//mettre la commande ici
+if (command == "serveur") {
+	request('http://lescookies.ml', function (error, response, body) {
+  console.log('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('body:', body); // Print the HTML for the Google homepage.
+});
+}
+
+//fin
+}
+module.exports.help = {
+  name: "serveur"
+}
