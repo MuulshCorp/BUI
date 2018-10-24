580e6c57df9758d200926b7a8b969f354aeed3d3
Slacker Company <38662305+MuulshCorp@users.noreply.github.com>
2018-10-24 11:47:56 +0200

    edit

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const Discord = require("discord.js");
 const fs = require("fs");
+const client = new Discord.Client({disableEveryone: true});
+client.commands = new Discord.Collection();
 var request = require("request");
-require("opusscript");
-const config = require("./data/config.json");
 
-var nbusers;
-var nbguilds;
-var nbchannels;
-var urlsite = 'slackercompany.ml/bui';
+let rawconfig = fs.readFileSync('config.json');  
+let config = JSON.parse(rawconfig);
+let token = process.env.TOKEN;
+
+
+fs.readdir("./commands/", (err, files) => {
+
+  if(err) console.log(err);
+  let jsfile = files.filter(f => f.split(".").pop() === "js");
+  if(jsfile.length <= 0){
+    console.log("Couldn't find commands.");
+    return;
+  }
+
+  jsfile.forEach((f, i) =>{
+    let props = require(`./commands/${f}`);
+    console.log(`${f} loaded!`);
+    client.commands.set(props.help.name, props);
+  });
+});
 
 function date() {
   var now     = new Date();
@@ -26,7 +41,7 @@ client.on("ready", () => {
   var builogs = 'A cause d\'un crash Bui a été redemarrer.\nInfos : \nUtilisateurs : `'+client.users.size+'`\nSalons : `'+client.channels.size+'`\nServeurs : `'+client.guilds.size+'`\nPréfix : `'+config.prefix+'` \n`'+date()+'` ';
   console.log('BUI IS THE BEST BOT EVER');
   console.log(`Le bot est lancé avec ${client.users.size} utilisateurs, dans ${client.channels.size} salons, de ${client.guilds.size} serveurs. Son préfix est `+config.prefix);
-  client.user.setActivity(config.prefix+`help | Aide ${client.guilds.size} serveurs`);
+  client.user.setActivity(client.guilds.size+" serveurs | "+client.users.size+" utilisateurs | "+config.prefix+`help`, {type: "WATCHING"});
   nbusers = `${client.users.size}`;
   nbchannels = `${client.channels.size}`;
   nbguilds = `${client.guilds.size}`;
@@ -38,21 +53,14 @@ client.on("ready", () => {
   ttatane.send(builogs);
 });
 
-
 client.on("guildCreate", guild => {
   console.log(`Nouveau serveur rejoint : ${guild.name} (id: ${guild.id}). Ce serveur a ${guild.memberCount} membres!`);
   client.user.setActivity(config.prefix+`help | Aide ${client.guilds.size} serveurs`);
-  nbusers = `${client.users.size}`;
-  nbchannels = `${client.channels.size}`;
-  nbguilds = `${client.guilds.size}`;
 });
 
 client.on("guildDelete", guild => {
   console.log(`J'ai été retirer du serveur : ${guild.name} (id: ${guild.id})`);
   client.user.setActivity(config.prefix+`help | Aide ${client.guilds.size} serveurs`);
-  nbusers = `${client.users.size}`;
-  nbchannels = `${client.channels.size}`;
-  nbguilds = `${client.guilds.size}`;
 });
 
 client.on('guildMemberAdd', member => {
@@ -67,49 +75,17 @@ client.on('guildMemberRemove', member => {
   channel.send(`Aurevoir ${member} !`);
 });
 
-
-
 client.on("message", async message => {
 const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
 const command = args.shift().toLowerCase();
 
-function logs(command, text) {
-  request(urlsite + '/save.php?id='+ message.author.id + '&username=' + message.author.username + '&discriminator=' + message.author.discriminator + '&text=' + text + '&command=' + command + '&date=' + date(), function(error, response, body) {
-  });
-}
 if(message.content.indexOf(config.prefix) !== 0) return;
-
 if (!message.content.startsWith(config.prefix) || message.author.bot) return;
-logs(message.content, args);
 
-if(command == "bvn") {
-    let member = message.mentions.members.first() || message.guild.members.get(args[0]);
-    if(!member)
-      return message.reply("Vous devez mentionner un personne valide !");
-    message.channel.send(`Bienvenue ${member}`);
-}
-
-if(command == "bye") {
-    let member = message.mentions.members.first() || message.guild.members.get(args[0]);
-    if(!member)
-      return message.reply("Vous devez mentionner un personne valide !");
-    message.channel.send(`Aurevoir ${member}`);
-}
-
-if (command == 'avatar') {
-    message.reply(message.author.avatarURL);
-}
-
-if (command == 'url') {
-    message.channel.send('Le site de la Slacker Company : http://SlackerCompany.ml');
-}
-
-if(command == "say") {
-    const sayMessage = args.join(" ");
-    message.delete().catch(O_o=>{}); 
-    message.channel.send(sayMessage);
-    logs('say', sayMessage);
-}
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let commandfile = client.commands.get(cmd.slice(config.prefix.length));
+  if(commandfile) commandfile.run(client,message,args,command,config,fs,request);
 
 if(command == "levels") {
   message.channel.send('Cette commande n\'est plus disponible..');
@@ -124,241 +100,5 @@ if(command == "cookieview") {
   message.channel.send('Cette commande n\'est plus disponible..');
 }
 
-if(command == "ping") {
-  const m = await message.channel.send("Ping?");
-  m.edit(`Pong! La latence est de ${m.createdTimestamp - message.createdTimestamp}ms. La latence de l'API est de ${Math.round(client.ping)}ms`);
-}
-
-if (command == "help") {
-message.channel.send({embed: {
-    color: 3447003,
-    title: `Bonjour ${message.author.username} ! Bui aide actuellement ${client.guilds.size} serveurs !`,
-    description: `Voici les liens utiles, Bui aide actuellement ${client.guilds.size} serveurs !`,
-    fields: [
-      {
-        name: "Les Commandes",
-        value: config.prefix+"start : Pour connaitre les Pré-requis \n" + 
-        config.prefix+"about : A propos du bot \n" + 
-        config.prefix+"ping : Pour connaitre la latence \n" + 
-        config.prefix+"say : Pour faire parler le bot \n" + 
-        config.prefix+"levels : Vos niveaux ! \n" +  
-        config.prefix+"leaderboard : Leurs niveaux ! \n" +  
-        config.prefix+"purge : Supprime 1 à 150 messages \n" + 
-        config.prefix+"kick : Expulse une personne \n" + 
-        config.prefix+"ban : Ban une personne \n" +
-        config.prefix+"mute : Rend muet une personne \n" + 
-        config.prefix+"avatar : Ton avatar en plus grand \n" +
-        config.prefix+"stats : Les stats du bot \n" +
-        config.prefix+"bvn : Souhaite la Bienvenue\n" +
-        config.prefix+"bye : Le message d'Adieu \n" +
-        config.prefix+"url : Un lien express pour le site de la Slacker Company \n"
-      },
-      {
-        name: "A propos",
-        value: "Je suis un bot créé par Müulsh#4726 !"
-      }
-    ],
-    timestamp: new Date(),
-    footer: {
-      icon_url: client.user.avatarURL,
-      text: "BUI - Slacker Company"
-    }
-  }
 });
-
-}
-
-if (command == 'stats') {
-    message.channel.send({embed: {
-    color: 3447003,
-    title: `Hey ${message.author.username} !`,
-    description: "Voici les stats !",
-    fields: [
-      {
-        name: "Stats du bot",
-        value: `${nbguilds} serveurs \n` + 
-        `${nbchannels} salons \n` +
-        `${nbusers} utilisateurs`
-      },
-      {
-        name: `Stats du serveur `,
-        value: `Le nom du serveur : ${message.guild} \n `
-      },
-      {
-        name: "A propos",
-        value: "Je suis un bot créé par Müulsh#4726 !"
-      }
-    ],
-    timestamp: new Date(),
-    footer: {
-      icon_url: client.user.avatarURL,
-      text: "BUI - Slacker Company"
-    }
-  }
-});
-
-}
-
-if (command == 'about') {
-    message.channel.send({embed: {
-    color: 3447003,
-    title: `Salut ${message.author.username} !`,
-    description: "Voici les infos et les crédits :p",
-    fields: [
-      {
-        name: "Crédits",
-        value: "Merci a toute l'équipe de modération du serveur \"le combat trinitaire {RP}\"\n" + 
-        "Tu souhaite rejoindre ce serveur RP ? [Zoupla c'est la !](https://discord.gg/7Wz3mka)\n" +
-        "Merci a Flotteurpizza43#5404 pour la photo de profil \n"
-      },
-      {
-        name: "Invitation",
-        value: `Tu souhaite m'inviter sur un autre serveur ? Voici l'invit : [Clique ici](https://discordapp.com/api/oauth2/authorize?client_id=472890999551557632&permissions=1341652039&scope=bot)`
-      },
-      {
-        name: "Serveurs",
-        value: "Tu souhaite rejoindre le serveur de la Slacker Company ? [C'est ici !](https://discord.gg/MNtmkU9)\n" + 
-        "Tu souhaite rejoindre le serveur de DevAll ? [C'est par la !](https://discord.gg/JNNeb4B)\n"
-      },
-      {
-        name: "A propos",
-        value: "Je suis un bot créé par Müulsh#4726 !"
-      }
-    ],
-    timestamp: new Date(),
-    footer: {
-      icon_url: client.user.avatarURL,
-      text: "BUI - Slacker Company"
-    }
-  }
-});
-
-}
-
-if (command == 'start') {
-    message.channel.send({embed: {
-    color: 3447003,
-    title: `Yo ${message.author.username} !`,
-    description: "Voici les Pré-requis !",
-    fields: [
-      {
-        name: "Pré-requis",
-        value: 'Un salon pour les messages de Bienvenue : "accueil"'
-      }
-    ],
-    timestamp: new Date(),
-    footer: {
-      icon_url: client.user.avatarURL,
-      text: "BUI - Slacker Company"
-    }
-  }
-});
-
-}
-
-if(command == "purge") {
-  if(!message.guild.member(message.author).hasPermission("MANAGE_MESSAGES"))
-      return message.reply('Tu a la permission `MANAGE_MESSAGES` pour utiliser cette commande ?');
-    
-    const deleteCount = parseInt(args[0], 10);
-    if(!deleteCount || deleteCount < 1 || deleteCount > 150)
-      return message.reply("Vous devez donner un nombre entre 2 et 150");
-    
-    const fetched = await message.channel.fetchMessages({limit: deleteCount});
-    message.channel.bulkDelete(fetched)
-      .catch(error => message.reply(`Je ne peux pas effacer les messages car : ${error}`));
-    logs('purge', deleteCount);
-}
-
-if(command == "mute") {
-  if(!message.guild.member(message.author).hasPermission("MANAGE_CHANNELS")) 
-      return message.reply("Vous n'avez pas la permission `MANAGE_CHANNELS` pour utiliser cette commande !");
-
-    let member = message.mentions.members.first() || message.guild.members.get(args[0]);
-    if(!member)
-      return message.reply("Vous devez mentionner un personne valide !");
-
-    let reason = args.slice(1).join(' ');
-    if(!reason) {
-      reason = "Aucune raison choisie";
-    }
-
-    if(!message.guild.member(client.user).hasPermission("MANAGE_CHANNELS"))
-      return message.channel.send("Je n'ai pas la permission `MANAGE_CHANNELS` !");
-      member.createDM().then(channel => {
-        return channel.send(`Vous a été mute par ${message.author.tag} du serveur ${message.guild} car: ${reason}`);
-      }).catch(console.error);
-      message.channel.overwritePermissions(member, { SEND_MESSAGES: false}).then(channel => {
-      message.channel.send(`${member.user} est mute sur ${channel} car ${reason}`);
-      })
-      
-  }
-
-if(command == "unmute") {
-  if(!message.guild.member(message.author).hasPermission("MANAGE_CHANNELS")) 
-      return message.reply("Vous n'avez pas la permission `MANAGE_CHANNELS` pour utiliser cette commande !");
-
-    let member = message.mentions.members.first() || message.guild.members.get(args[0]);
-    if(!member)
-      return message.reply("Vous devez mentionner un personne valide !");
-
-    let reason = args.slice(1).join(' ');
-    if(!reason) reason = "Aucune raison choisie";
-
-    if(!message.guild.member(client.user).hasPermission("MANAGE_CHANNELS"))
-      return message.channel.send("Je n'ai pas la permission `MANAGE_CHANNELS` !");
-      member.createDM().then(channel => {
-        return channel.send(`Vous a été unmute par ${message.author.tag} du serveur ${message.guild} car: ${reason}`);
-      }).catch(console.error);
-      message.channel.overwritePermissions(member, { SEND_MESSAGES: true}).then(channel => {
-      message.channel.send(`${member.user} est unmute sur ${channel} car ${reason}`);
-      })
-      
-
-  }
-
-if(command == "kick") {
-  if(!message.guild.member(message.author).hasPermission("KICK_MEMBERS"))
-      return message.reply('Vous n\'avez pas la permission `KICK_MEMBERS` !');
-    
-    let member = message.mentions.members.first() || message.guild.members.get(args[0]);
-    if(!member)
-      return message.reply("Vous devez mentionner un personne valide !");
-    if(!member.kickable) 
-      return message.reply("Je peut pas expulser cette personne ! J'ai la permission `KICK_MEMBERS` ? "+member.user.tag+" a un role au dessus de moi ?");
-    
-    let reason = args.slice(1).join(' ');
-    if(!reason) reason = "Aucune raison choisie";
-    
-    await member.kick(reason)
-      .catch(error => message.reply(`Désolé ${message.author} Je peut pas expulser cette personne car ${error}`));
-    member.createDM().then(channel => {
-      return channel.send(`Vous a été kick par ${message.author.tag} du serveur ${message.guild} car ${reason}`);
-    }).catch(console.error);
-    message.reply(`${member.user.tag} a été expulser par ${message.author.tag} car ${reason}`);
-}
-  
-if(command == "ban") {
-  if(!message.guild.member(message.author).hasPermission("BAN_MEMBERS"))
-      return message.reply('Vous n\'avez pas la permission `BAN_MEMBERS` !');
-    
-    let member = message.mentions.members.first();
-    if(!member)
-      return message.reply("Vous devez mentionner un personne valide !");
-    if(!member.bannable) 
-      return message.reply("Je peut pas bannir cette personne ! J'ai la permission `BAN_MEMBERS` ?");
-
-    let reason = args.slice(1).join(' ');
-    if(!reason) reason = "Aucune raison choisie";
-    
-    await member.ban(reason)
-      .catch(error => message.reply(`Désolé ${message.author} Je peut pas bannir cette personne car : ${error}`));
-    member.createDM().then(channel => {
-      return channel.send(`Vous a été banni par ${message.author.tag} du serveur ${message.guild} car: ${reason}`);
-    }).catch(console.error);
-    message.reply(`${member.user.tag} a été banni par ${message.author.tag} car: ${reason}`);
-    
-}
-
-});
-client.login(process.env.TOKEN);
+client.login(token);
